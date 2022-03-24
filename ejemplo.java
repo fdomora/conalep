97d17921af65c61b9381db54be37191ee81b17d8
Fernando Mora <fmora.utch@gmail.com>
2022-03-23 19:24:30 -0700

    nuevo cambio

--- ejemplo.java
+++ ejemplo.java
@@ -1,6 +1,7 @@
 class ejemplo{
    public static void main(String[] args){
        System.out.println("Hola Mundo, saludos Fernando");
+       System.out.println("La conexion con Github se ha hecho");
   }
 
 }
